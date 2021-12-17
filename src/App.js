e173bc16a8c95345e847e9842c434065bb534280
Brett Smith <brettsmith212@gmail.com>
2021-12-16 19:05:33 -0800

    Navbar created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from "react";
+import styled from "styled-components";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./Theme";
+
+const Navbar = styled.nav`
+  width: 100%;
+  height: 6rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid red;
+
+  h2 {
+    font-size: ${({ theme }) => theme.fontSize.h2};
+    margin-left: 3rem;
+  }
+
+  a {
+    font-size: ${({ theme }) => theme.fontSize.a};
+    text-decoration: none;
+    color: black;
+    margin-right: 3rem;
+    cursor: pointer;
+  }
+`;
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <Navbar>
+          <h2>Lambda Eats</h2>
+          <a href="#">Home</a>
+        </Navbar>
+      </>
+    </ThemeProvider>
   );
 };
 export default App;
---
+++ src/Theme.js
@@ -0,0 +1,21 @@
+export const theme = {
+  colors: {
+    header: "#212529",
+    body: "#212529",
+    section: "#212529",
+    card: "#495057",
+    footer: "#dee2e6",
+    fontColor: "#fff",
+    hr: "#868e96",
+    modal: "rgba(0, 0, 0, 0.8)",
+    voted: "#40c057",
+  },
+  fontSize: {
+    input: "2.2rem",
+    h1: "4.5rem",
+    h2: "3rem",
+    h3: "2rem",
+    h4: "1.6rem",
+    a: "2rem",
+  },
+};
